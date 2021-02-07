4ee0c6e874c7d447ae179c5854f5cd2ac89971d9
tcwofs <liampicave@gmail.com>
2021-02-07 14:08:47 +0300

    build: add new dependencies, create .env.example, update linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    quotes: ['error', 'single'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'comma-dangle': ['error', 'never']
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 80,
+  jsxSingleQuote: true,
+  singleQuote: true,
+  trailingComma: 'none',
 };
